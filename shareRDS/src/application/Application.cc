73f77263a495e694f3d0d702e0b0343d9e21bd88
Andrea Mambretti <m4mbr3@gmail.com>
2013-05-13 22:05:03 +0200

    Abstracted number of replicas into client comp module

--- shareRDS/src/application/Application.cc
+++ shareRDS/src/application/Application.cc
@@ -52,10 +52,8 @@ SystemMsg* Application::generateMessage(){
     ttmsg->setClientID(clientID);
     ttmsg->setOperation(intuniform(0,1));
     ttmsg->setData(intuniform(-1000, 1000));
-    //ttmsg->setDataID(((intuniform(0,100)%2)==0) ?(const char *) 'a' + rand() % (('z'-'a') + 1): (const char *)'A' + rand() % (('Z'-'A') + 1));
     char buffer[33];
     ttmsg->setDataID((intuniform(0,100)%2) == 0 ? itoa((intuniform(0,25)+'a'),buffer,10): itoa((intuniform(0,25)+'A'),buffer,10));
-    std::cout << ttmsg->getDataID() << std::endl;
     return ttmsg;
 }
 int Application::getClientID()
