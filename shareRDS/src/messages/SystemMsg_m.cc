a5bdbb45d7d96568528dc873781a05360a5403a1
Andrea Mambretti <m4mbr3@gmail.com>
2013-05-29 21:44:08 +0200

    changes

--- shareRDS/src/messages/SystemMsg_m.cc
+++ shareRDS/src/messages/SystemMsg_m.cc
@@ -1,5 +1,5 @@
 //
-// Generated file, do not edit! Created by opp_msgc 4.2 from messages/SystemMsg.msg.
+// Generated file, do not edit! Created by opp_msgc 4.3 from messages/SystemMsg.msg.
 //
 
 // Disable warnings about unused variables, empty switch stmts, etc:
